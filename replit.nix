d9fb6c0145236984448dd96c3d575335b6f558ce
vocode <21545364-vocode@users.noreply.replit.com>
2024-01-27 23:48:01 +0000

    Working Version
    Working Version Azure

--- replit.nix
+++ replit.nix
@@ -1,7 +1,20 @@
 {pkgs}: {
   deps = [
+    pkgs.neovim
     pkgs.libxcrypt
     pkgs.ffmpeg-full
     pkgs.libuuid
+    pkgs.openssl_1_1
   ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.libuuid
+      pkgs.alsa-lib
+      pkgs.openssl_1_1
+    ];
+    NIXPKGS_ALLOW_INSECURE="1";
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.libuuid
+    ];
+  };
 }
